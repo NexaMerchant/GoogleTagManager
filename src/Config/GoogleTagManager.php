63843985277b65529a21ee005d9c2dec028aa759
Steve <34465153+xxl4@users.noreply.github.com>
2024-08-12 18:13:47 +0800

    fix update config

--- src/Config/GoogleTagManager.php
+++ src/Config/GoogleTagManager.php
@@ -23,4 +23,69 @@ return [
      * The version number of the package
      */
     'versionNum' => '100',
+
+    /**
+     * 
+     * The description of the package
+     */
+    'description' => 'Google Tag Manager is a tag management system that allows you to quickly and easily update tags and code snippets on your website or mobile app, such as those intended for traffic analysis and marketing optimization. You can add and update AdWords, Google Analytics, Floodlight, and non-Google tags from the Google Tag Manager user interface instead of editing site code.',
+
+    /**
+     * 
+     * The author of the package
+     */
+    'author' => 'Steve',
+
+    /**
+     * 
+     * The email of the author
+     */
+    'email' => 'nice.lizhi@gmail.com',
+
+    /**
+     * 
+     * The homepage of the package
+     */
+    'homepage' => 'https://github.com/xxl4',
+
+    /**
+     * 
+     * The keywords of the package
+     */
+    'keywords' => [
+        'Google Tag Manager',
+        'Google',
+        'Tag Manager',
+        'Google Analytics',
+        'AdWords',
+        'Floodlight',
+        'Google Tag Manager user interface',
+        'tag management system',
+        'traffic analysis',
+        'marketing optimization',
+        'code snippets',
+        'website',
+        'mobile app',
+    ],
+
+    /**
+     * 
+     * The license of the package
+     */
+    'license' => 'MIT',
+
+    /**
+     * 
+     * The type of the package
+     */
+    'type' => 'library',
+
+    /**
+     * 
+     * The support of the package
+     */
+    'support' => [
+        'email' => 'nice.lizhi@gmail.com',
+        'issues' => 'https://github.com/xxl4'
+    ],
 ];
